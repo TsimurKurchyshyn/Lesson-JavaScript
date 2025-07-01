e0832d9a270c8dc7bc69491164dbfe84fa623051
Tsimur <dopingjr2010@gmail.com>
2025-07-01 16:32:37 -0400

    First book lesson 1

---
+++ main.js
@@ -0,0 +1,12 @@
+setTimeout(wakeUpUser, 5000);
+    function wakeUpUser() {
+        alert("Are you going to stare at this boring page forever?");
+}
+
+var age = 25;
+var name = "Owen";
+if (age > 14) {
+ alert("Sorry this page is for kids only!");
+} else {
+ alert("Welcome " + name + "!");
+}
